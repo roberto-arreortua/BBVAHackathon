03a3c254ab0521fef28e1feba0a611ca15e4dff7
roberto-arreortua <robertoarreortua@gmail.com>
2020-10-17 11:56:21 -0500

    second

--- HackBBVA/settings.py
+++ HackBBVA/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'Users.Users'
 MEDIA_URL = '/media/'
